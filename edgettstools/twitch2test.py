7261c2dea513c40173a1a1b68cc7021edf6e9e25
Jorge PV <94359345+akthanon@users.noreply.github.com>
2024-06-22 19:25:10 -0700

    Update twitch2test.py

--- edgettstools/twitch2test.py
+++ edgettstools/twitch2test.py
@@ -34,7 +34,7 @@ def save_messages_to_file_h(messages_h):
 
 def limpiar_texto(texto):
     # Expresión regular para mantener letras, números, espacios, acentos, ñ y signos de puntuación
-    patron = r'[^a-zA-Z0-9\sáéíóúüñÁÉÍÓÚÜÑ:\[\].,!?¿¡]'
+    patron = r'[^a-zA-Z0-9\sáéíóúüñÁÉÍÓÚÜÑ-_:\[\].,!?¿¡]'
     texto_limpio = re.sub(patron, '', texto)
     return texto_limpio
 
